d063aa0f7487d01632130cf0ec7730ee9deb6b17
mgreen4 <m.verde@relewant.com>
2021-02-21 19:38:42 +0100

    mobile UI fix

--- src/quotation/quotation.js
+++ src/quotation/quotation.js
@@ -4,7 +4,7 @@ import {
   getBaseModules,
   getViewMode,
   getResources,
-  getProfessionals
+  getProfessionals,
 } from "../store/selectors/genericSelectors";
 import {
   getSelectedQuotationId,
@@ -12,7 +12,7 @@ import {
 } from "../store/selectors/quotationSelectors";
 import {
   getSelectedProjectId,
-  getProject
+  getProject,
 } from "../store/selectors/projectSelectors";
 import {
   selectProject,
@@ -22,7 +22,7 @@ import {
   addQuotation,
   startNewQuotation,
   editSelectedQuotationCode,
-  editQuotation
+  editQuotation,
 } from "../store/actions/quotationActions";
 
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -53,14 +53,13 @@ const NewQuotation = ({
   editQuotationCode,
   startNewQuotation,
   professionals,
-  saveQuotationEdit
+  saveQuotationEdit,
 }) => {
-
-  const [quotationCode, setQuotationCode] = useState('');
+  const [quotationCode, setQuotationCode] = useState("");
 
   useEffect(() => {
     const query = new URLSearchParams(history.location.search);
-    const locationToken = history.location.pathname.split('/');
+    const locationToken = history.location.pathname.split("/");
     const location = locationToken[locationToken.length - 1];
     const queryProject = query.get("project");
     const queryQuotation = query.get("quotation");
@@ -69,9 +68,8 @@ const NewQuotation = ({
     if (!selectedProjectId) {
       if (queryProject) {
         chooseProject(queryProject);
-      }
-      else {
-        history.push('/');
+      } else {
+        history.push("/");
       }
     } else {
       if (!project || project.id !== selectedProjectId) {
@@ -80,7 +78,7 @@ const NewQuotation = ({
         if (!selectedQuotationId && location !== "new-quotation") {
           chooseQuotation(queryQuotation);
         }
-        if (selectedQuotationId !== '0' && location === "new-quotation") {
+        if (selectedQuotationId !== "0" && location === "new-quotation") {
           startNewQuotation(queryQuotationType);
         }
       }
@@ -93,8 +91,7 @@ const NewQuotation = ({
 
   useEffect(() => {
     let collapsible = document.querySelectorAll(".collapsible");
-    if (collapsible)
-      M.Collapsible.init(collapsible, { accordion: false });
+    if (collapsible) M.Collapsible.init(collapsible, { accordion: false });
   });
 
   useEffect(() => {
@@ -133,42 +130,11 @@ const NewQuotation = ({
     let rts = document.querySelectorAll(".resourcesTrigger");
     let acs = document.querySelectorAll(".activityComment");
     let qcs = document.querySelectorAll(".qtCost");
-    if (pt.classList.contains("scale-out")) {
-      qg.classList.remove("s12");
-      qg.classList.add("s8", "m8", "l9");
-
-      checks.forEach((check) => {
-        check.classList.remove("s5", "m2");
-      });
-      checks.forEach((check) => {
-        check.classList.add("s6", "m3");
-      });
-
-      rts.forEach((rt) => {
-        rt.classList.remove("side-by-side");
-      });
-      rts.forEach((rt) => {
-        rt.classList.add("truncate", "center");
-      });
-
-      acs.forEach((ac) => {
-        ac.classList.remove("m6");
-      });
-      acs.forEach((ac) => {
-        ac.classList.add("m4", "s-space-up");
-      });
-
-      qcs.forEach((ac) => {
-        ac.classList.remove("m4");
-      });
-
-      pt.classList.remove("scale-out");
-      pt.classList.add("scale-in");
-    } else {
+    if (pt.classList.contains("scale-in")) {
       pt.classList.remove("scale-in");
-      pt.classList.add("scale-out");
+      // pt.classList.add("scale-out");
 
-      qg.classList.remove("s8", "m8", "l9");
+      qg.classList.remove("m8", "l9");
       qg.classList.add("s12");
 
       checks.forEach((check) => {
@@ -195,6 +161,46 @@ const NewQuotation = ({
       qcs.forEach((ac) => {
         ac.classList.add("m4");
       });
+    } else {
+      qg.classList.add("m8", "l9");
+
+      checks.forEach((check) => {
+        check.classList.remove("s5", "m2");
+      });
+      checks.forEach((check) => {
+        check.classList.add("s6", "m3");
+      });
+
+      rts.forEach((rt) => {
+        rt.classList.remove("side-by-side");
+      });
+      rts.forEach((rt) => {
+        rt.classList.add("truncate", "center");
+      });
+
+      acs.forEach((ac) => {
+        ac.classList.remove("m6");
+      });
+      acs.forEach((ac) => {
+        ac.classList.add("m4", "s-space-up");
+      });
+
+      qcs.forEach((ac) => {
+        ac.classList.remove("m4");
+      });
+
+      // pt.classList.remove("scale-out");
+      pt.classList.add("scale-in");
+    }
+  };
+
+  const toggleSmallPeopleTable = (evt) => {
+    evt.preventDefault();
+    let pt = document.getElementById("smallPeopleTable");
+    if (pt.classList.contains("hide")) {
+      pt.classList.remove("hide");
+    } else {
+      pt.classList.add("hide");
     }
   };
 
@@ -202,18 +208,22 @@ const NewQuotation = ({
     e.preventDefault();
     if (viewMode === VIEW_MODES.CREATE) {
       saveQuotationToDb();
-      history.push(`/project/quotations/?project=${project.id}`)
+      history.push(`/project/quotations/?project=${project.id}`);
     } else {
       saveQuotationEdit();
     }
-
   };
 
   const checkCreateDisabled = () => {
     if (!selectedQuotation) return true;
     if (!selectedQuotation.code) return true;
-    if (!selectedQuotation.modules || selectedQuotation.modules.length === 0) return true;
-    if (selectedQuotation.quotationType === QUOTATION_TYPES.PROVIDER && !selectedQuotation.provider) return true
+    if (!selectedQuotation.modules || selectedQuotation.modules.length === 0)
+      return true;
+    if (
+      selectedQuotation.quotationType === QUOTATION_TYPES.PROVIDER &&
+      !selectedQuotation.provider
+    )
+      return true;
 
     return false;
   };
@@ -226,119 +236,138 @@ const NewQuotation = ({
   const titleWidth = viewMode === VIEW_MODES.CREATE ? "m12" : "m11";
   return (
     <div>
-      {
-        selectedQuotation ?
-          <div id="selectedQuotation" className="section">
-            <div>
-              <form className="white" onSubmit={(e) => saveQuotation(e)}>
-                <div className="container">
-                  <div className="row">
-                    <QuotationActions />
-                    <div className={`input-field col s12 ${titleWidth}`}>
-                      <label className="active" htmlFor="quotationCode">
-                        quotation code
-                      </label>
-                      <input
-                        type="text"
-                        name="quotationCode"
-                        value={quotationCode}
-                        onChange={(e) => editQuotationCodeInput(e.target.value)}
-                        disabled={viewMode === VIEW_MODES.VIEW ? true : null}
-                      ></input>
-                    </div>
+      {selectedQuotation ? (
+        <div id="selectedQuotation" className="section">
+          <div>
+            <form className="white" onSubmit={(e) => saveQuotation(e)}>
+              <div className="container">
+                <div className="row">
+                  <QuotationActions />
+                  <div className={`input-field col s12 ${titleWidth}`}>
+                    <label className="active" htmlFor="quotationCode">
+                      quotation code
+                    </label>
+                    <input
+                      type="text"
+                      name="quotationCode"
+                      value={quotationCode}
+                      onChange={(e) => editQuotationCodeInput(e.target.value)}
+                      disabled={viewMode === VIEW_MODES.VIEW ? true : null}
+                    ></input>
                   </div>
-                  <div className="row">
-                    {
-                      selectedQuotation.quotationType === "PROVIDER" ?
-                        <div>
-                          <Provider />
-                        </div> :
-                        <div>
-                        </div>
-                    }
-                  </div>
-                  {viewMode === VIEW_MODES.CREATE || viewMode === VIEW_MODES.EDIT ?
-                    <NewModule />
-                    : ''}
                 </div>
                 <div className="row">
-                  <div className="col s1 offset-s11">
-                    <a
-                      href="#!"
-                      id="peopleTableTrigger"
-                      onClick={(evt) => togglePeopleTable(evt)}
-                    >
-                      <i
-                        className="material-icons indigo-text right"
-                        title="People Fees Table"
-                      >
-                        assignment_ind
-                    </i>
-                    </a>
-                  </div>
+                  {selectedQuotation.quotationType === "PROVIDER" ? (
+                    <div>
+                      <Provider />
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
                 </div>
-
-                <div className="row">
-                  <div className="col s12" id="quotationGroup">
-                    <ul className="collapsible">
-                      {selectedQuotation.modules.map((module) => (
-                        <Module
-                          key={module.id}
-                          module={module}
-                          viewMode={viewMode}
-                          quotationType={selectedQuotation.quotationType}
-                        />
-                      ))}
-                    </ul>
-                    <div className="col s12 m4 l4 z-depth-1 qtCost">
-                      <h6 className="bolder price center">
-                        Price without PT {selectedQuotation.quotationNotPTCost || 0}
-                      </h6>
-                    </div>
-                    <div className="col s12 m4 l4 z-depth-1 qtCost">
-                      <h6 className="bolder price center">
-                        PT only {selectedQuotation.quotationPTCost || 0}
-                      </h6>
-                    </div>
-                    <div className="col s12 m4 l4 z-depth-1 qtCost">
-                      <h6 className="bolder price center">
-                        Quotation cost {selectedQuotation.quotationCost || 0}
-                      </h6>
-                    </div>
-                    {viewMode === VIEW_MODES.VIEW ? null : (
-                      <div className="input-field col s12 center space-up">
-                        <button
-                          className="btn indigo lighten-1 z-depth-0"
-                          type="submit"
-                          disabled={checkCreateDisabled()}
-                        >
-                          {viewMode === VIEW_MODES.CREATE ? "Create" : "Save"}
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                  <div
-                    className="col s4 m4 l3 scale-transition scale-out"
-                    id="peopleTable"
+                {viewMode === VIEW_MODES.CREATE ||
+                viewMode === VIEW_MODES.EDIT ? (
+                  <NewModule />
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="row">
+                <div className="col s1 hide-on-small-only offset-s11">
+                  <a
+                    href="#!"
+                    id="peopleTableTrigger"
+                    onClick={(evt) => togglePeopleTable(evt)}
                   >
-                    <ResourceList resources={resources} />
+                    <i
+                      className="material-icons indigo-text right"
+                      title="People Fees Table"
+                    >
+                      assignment_ind
+                    </i>
+                  </a>
+                </div>
+                <div className="col s1 offset-s11 hide-on-med-and-up">
+                  <a href="#!" id="smallPeopleTableTrigger" onClick={(evt) => toggleSmallPeopleTable(evt)}>
+                    <i
+                      className="material-icons indigo-text right"
+                      title="People Fees Table"
+                    >
+                      assignment_ind
+                    </i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="row">
+                <div
+                  className="col s12 hide hide-on-med-and-up"
+                  id="smallPeopleTable"
+                >
+                  <ResourceList resources={resources} />
+                </div>
+                <div className="col s12" id="quotationGroup">
+                  <ul className="collapsible">
+                    {selectedQuotation.modules.map((module) => (
+                      <Module
+                        key={module.id}
+                        module={module}
+                        viewMode={viewMode}
+                        quotationType={selectedQuotation.quotationType}
+                      />
+                    ))}
+                  </ul>
+                  <div className="col s12 m4 l4 z-depth-1 qtCost">
+                    <h6 className="bolder price center">
+                      Price without PT{" "}
+                      {selectedQuotation.quotationNotPTCost || 0}
+                    </h6>
                   </div>
+                  <div className="col s12 m4 l4 z-depth-1 qtCost">
+                    <h6 className="bolder price center">
+                      PT only {selectedQuotation.quotationPTCost || 0}
+                    </h6>
+                  </div>
+                  <div className="col s12 m4 l4 z-depth-1 qtCost">
+                    <h6 className="bolder price center">
+                      Quotation cost {selectedQuotation.quotationCost || 0}
+                    </h6>
+                  </div>
+                  {viewMode === VIEW_MODES.VIEW ? null : (
+                    <div className="input-field col s12 center space-up">
+                      <button
+                        className="btn indigo lighten-1 z-depth-0"
+                        type="submit"
+                        disabled={checkCreateDisabled()}
+                      >
+                        {viewMode === VIEW_MODES.CREATE ? "Create" : "Save"}
+                      </button>
+                    </div>
+                  )}
                 </div>
-              </form>
-              {viewMode !== VIEW_MODES.VIEW ? (
-                <div>
-                  <NewResource />
+                <div
+                  className="col m4 l3 scale-transition scale-out hide-on-small-only"
+                  id="peopleTable"
+                >
+                  <ResourceList resources={resources} />
                 </div>
-              ) : null}
-            </div>
+              </div>
+            </form>
+            {viewMode !== VIEW_MODES.VIEW ? (
+              <div>
+                <NewResource />
+              </div>
+            ) : null}
           </div>
-          : <div className="center valign-page-center">
-            <Preloader classes="preloader-wrapper big active" />
-          </div>
-      }
+        </div>
+      ) : (
+        <div className="center valign-page-center">
+          <Preloader classes="preloader-wrapper big active" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -349,7 +378,7 @@ const mapStateToProps = (state) => {
     selectedQuotationId: getSelectedQuotationId(state),
     viewMode: getViewMode(state),
     resources: getResources(state),
-    professionals: getProfessionals(state)
+    professionals: getProfessionals(state),
   };
 };
 
@@ -358,10 +387,12 @@ const mapDispatchToProps = (dispatch) => {
     chooseProject: (projectId) => dispatch(selectProject(projectId)),
     chooseQuotation: (quotationId) => dispatch(selectQuotation(quotationId)),
     loadProject: (projectId) => dispatch(loadProjectAction(projectId)),
-    saveQuotationToDb: (selectedQuotation) => dispatch(addQuotation(selectedQuotation)),
+    saveQuotationToDb: (selectedQuotation) =>
+      dispatch(addQuotation(selectedQuotation)),
     startNewQuotation: (type) => dispatch(startNewQuotation(type)),
-    editQuotationCode: (quotationCode) => dispatch(editSelectedQuotationCode(quotationCode)),
-    saveQuotationEdit: () => dispatch(editQuotation())
+    editQuotationCode: (quotationCode) =>
+      dispatch(editSelectedQuotationCode(quotationCode)),
+    saveQuotationEdit: () => dispatch(editQuotation()),
   };
 };
 
